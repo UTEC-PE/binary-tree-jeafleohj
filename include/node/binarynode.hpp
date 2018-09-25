9bb6a0291bd35597fac555f72d726f77260378ba
jeafleohj <jhony.angulo@utec.edu.pe>
2018-09-25 06:47:31 -0500

    Binary node class

---
+++ include/node/binarynode.hpp
@@ -0,0 +1,26 @@
+#ifndef BINARYNODE_HPP
+#define BINARYNODE_HPP
+
+#include "node.hpp"
+
+template <typename ST>
+struct BinaryNode: public Node<ST>{
+	typedef typename ST::T T;
+	BinaryNode* left;
+	BinaryNode* right;
+	BinaryNode(BinaryNode* left=nullptr, BinaryNode* right=nullptr)
+		: Node<ST>(), left(left), right(right)
+	{};
+	BinaryNode(T  data, BinaryNode* left=nullptr, BinaryNode* right=nullptr)
+		:  Node<ST>(data), left(left), right(right)
+	{};
+	void killSelf(){
+		if(left) left->killSelf();
+		if(right) right->killSelf();
+		delete this;
+	}
+	~BinaryNode(){
+		killSelf();
+	}
+};
+#endif
